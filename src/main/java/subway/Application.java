46e427b1628d4da61d29049b907b297a1d684b83
JaehongDev <println@kakao.com>
2022-12-07 22:13:12 +0900

    설정완료

---
+++ src/main/java/subway/Application.java
@@ -0,0 +1,45 @@
+package subway;
+
+import java.util.Scanner;
+
+public class Application {
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+
+         // 지우고 시작하시면 됩니다.
+         System.out.println("[INFO] 지하철 역이 등록되었습니다.");
+         System.out.println("[INFO] 교대역");
+         System.out.println("[INFO] 강남역");
+         System.out.println("[INFO] 역삼역");
+         System.out.println("[INFO] 남부터미널역");
+         System.out.println("[INFO] 양재역");
+         System.out.println("[INFO] 양재시민의숲역");
+         System.out.println("[INFO] 매봉역");
+         System.out.println("[INFO] 잠실역");
+         System.out.println("[INFO] 지하철 역이 삭제되었습니다.");
+         System.out.println("[INFO] 지하철 노선이 등록되었습니다.");
+         System.out.println("[INFO] 2호선");
+         System.out.println("[INFO] 3호선");
+         System.out.println("[INFO] 신분당선");
+         System.out.println("[INFO] 1호선");
+         System.out.println("[INFO] 지하철 노선이 삭제되었습니다.");
+         System.out.println("[INFO] 구간이 등록되었습니다.");
+         System.out.println("[INFO] 구간이 삭제되었습니다.");
+         System.out.println("[INFO] 2호선");
+         System.out.println("[INFO] ---");
+         System.out.println("[INFO] 교대역");
+         System.out.println("[INFO] 강남역");
+         System.out.println("[INFO] 역삼역");
+         System.out.println("[INFO] 3호선");
+         System.out.println("[INFO] ---");
+         System.out.println("[INFO] 교대역");
+         System.out.println("[INFO] 남부터미널역");
+         System.out.println("[INFO] 양재역");
+         System.out.println("[INFO] 매봉역");
+         System.out.println("[INFO] 신분당선");
+         System.out.println("[INFO] ---");
+         System.out.println("[INFO] 강남역");
+         System.out.println("[INFO] 양재역");
+         System.out.println("[INFO] 양재시민의숲역");
+    }
+}
