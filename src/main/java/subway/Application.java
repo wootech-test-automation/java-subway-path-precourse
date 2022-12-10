f55c4aa1e6c844c6a596c313f6152fdf343d21df
HiiWee <wpdlzhf159@gmail.com>
2022-12-10 13:40:58 +0900

    docs: 초기 기능목록 추가

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,9 +1,11 @@
 package subway;
 
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final Scanner scanner = new Scanner(System.in);
 
     }
 }
