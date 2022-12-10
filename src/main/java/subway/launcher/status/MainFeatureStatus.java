a1139e6751a0b8e92b82571b304a2edc42917492
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-10 16:29:04 +0900

    feat: add MainFeatureStatus

---
+++ src/main/java/subway/launcher/status/MainFeatureStatus.java
@@ -0,0 +1,21 @@
+package subway.launcher.status;
+
+import subway.launcher.SubwayLauncher;
+import subway.view.keyword.MainFeatureKeyword;
+
+public class MainFeatureStatus extends AbstractRunnableSubwayStatus {
+
+    public MainFeatureStatus(SubwayLauncher context) {
+        super(context);
+    }
+
+    @Override
+    public SubwayStatus process() {
+        MainFeatureKeyword mainFeatureKeyword = inputView.inputMainFeature();
+        if (mainFeatureKeyword.equals(MainFeatureKeyword.SEARCH)) {
+            return new PathFeatureStatus(context);
+        }
+        // quit
+        return new QuitStatus(context);
+    }
+}
