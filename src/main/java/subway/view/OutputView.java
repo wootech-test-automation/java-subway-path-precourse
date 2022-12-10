b367106cd3ffe2b08d6d1e7e3a393940aedd9292
JaehongDev <println@kakao.com>
2022-12-10 16:11:46 +0900

    feature (OutputView) : 출력 기능 추가

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,4 +1,20 @@
 package subway.view;
 
+import java.util.List;
+import subway.domain.ResultDto;
+import subway.domain.Station;
+
 public class OutputView extends IoPrinter{
+    public void printResult(List<Station> result) {
+        this.println(result);
+    }
+
+    public void printResult(ResultDto result) {
+        this.printTitle("조회 결과");
+        this.info("---");
+        this.info(String.format("총 거리: %dkm", result.getDistanceSum()));
+        this.info(String.format("총 소요 시간: %dkm", result.getTimeSum()));
+        this.info("---");
+        this.info(result.getStationName());
+    }
 }
