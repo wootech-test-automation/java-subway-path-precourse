b74625a51263176bda6237de02dadbe16da8eedc
JaehongDev <println@kakao.com>
2022-12-10 16:50:11 +0900

    feature (IoPrinter) : 기본 출력기능 구현

--- src/main/java/subway/view/IoPrinter.java
+++ src/main/java/subway/view/IoPrinter.java
@@ -1,10 +1,16 @@
 package subway.view;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class IoPrinter {
     private static final String INFO_FORMAT = "[INFO] %s";
     private static final String ERROR_FORMAT = "[ERROR] %s";
     private static final String TITLE_FORMAT = "## %s";
 
+    protected void println(){
+        this.println("");
+    }
     protected void println(final Object message){
         System.out.println(message);
     }
@@ -13,6 +19,9 @@ public class IoPrinter {
         this.println(String.format(TITLE_FORMAT, titleName));
     }
 
+    protected void info(final List<String> message){
+        this.println(message.stream().map(s -> String.format(INFO_FORMAT,s)).collect(Collectors.joining("\n")));
+    }
     protected void info(final Object message) {
         this.println(String.format(INFO_FORMAT, message));
     }
