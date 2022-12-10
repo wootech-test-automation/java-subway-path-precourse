d193ba14973c20c70398cda86faeed75253cc38d
JaehongDev <println@kakao.com>
2022-12-10 16:55:53 +0900

    feature (Line) : 선로에 대한 기능 구현

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,15 +1,26 @@
 package subway.domain;
 
-public class Line {
-    private String name;
+import java.util.List;
 
-    public Line(String name) {
+public class Line {
+    private final String name;
+    private final List<Station> stations;
+
+    public Line(String name, List<Station> stations) {
         this.name = name;
+        this.stations = stations;
     }
 
     public String getName() {
         return name;
     }
 
+    public List<Station> getStations() {
+        return stations;
+    }
+
     // 추가 기능 구현
+    public boolean compareOriginalStationIndexBeforeTargetStation(Station originalStation, Station targetStation ) {
+        return stations.indexOf(originalStation) < stations.indexOf(targetStation);
+    }
 }
