0bbe5bc47d2187f859f9a4e79108d2ca90627dbb
JaehongDev <println@kakao.com>
2022-12-10 16:56:32 +0900

    feautre (LineRepository) : 선로에 대한 기본값을 가지도록 변경

--- src/main/java/subway/domain/LineRepository.java
+++ src/main/java/subway/domain/LineRepository.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import subway.exception.InvalidInputException;
 
 public class LineRepository {
     private static final List<Line> lines = new ArrayList<>();
-
+    static {
+        LineRepository.addLine(new Line("2호선", List.of(new Station("교대역"), new Station("강남역"), new Station("역삼역"))));
+        LineRepository.addLine(new Line("3호선", List.of
+                (new Station("교대역"), new Station("남부터미널역"), new Station("양재역"), new Station("매봉역")))
+        );
+        LineRepository.addLine(new Line("신분당선",
+                List.of(new Station("강남역"), new Station("양재역"), new Station("양재시민의숲역")))
+        );
+    }
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
     }
@@ -23,4 +32,10 @@ public class LineRepository {
     public static void deleteAll() {
         lines.clear();
     }
+
+    public static void existsSameLine(Station upStation, Station terminalStation) {
+        var result = lines().stream().noneMatch(line -> line.compareOriginalStationIndexBeforeTargetStation(upStation, terminalStation));
+    }
+
+
 }
