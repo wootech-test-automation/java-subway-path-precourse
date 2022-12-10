b96df9e5e54ab74f82716ad4f42f461aa902c6b1
JaehongDev <println@kakao.com>
2022-12-10 16:55:12 +0900

    feature (SectionRepository) : 구간에대한 모든 정보를 가지도록 변경

---
+++ src/main/java/subway/domain/section/SectionRepository.java
@@ -0,0 +1,32 @@
+package subway.domain.section;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import subway.domain.Station;
+import subway.domain.weight.WeightCode;
+import subway.domain.weight.WeightDivision;
+
+public class SectionRepository {
+    private static final List<Section> sections = new ArrayList<>();
+
+    static {
+        sections.add(new Section(new Station("교대역"), new Station("강남역"), new WeightDivision("2/3")));
+        sections.add(new Section(new Station("강남역"), new Station("역삼역"), new WeightDivision("2/3")));
+
+        sections.add(new Section(new Station("교대역"), new Station("남부터미널역"), new WeightDivision("3/2")));
+        sections.add(new Section(new Station("남부터미널역"), new Station("양재역"), new WeightDivision("6/5")));
+        sections.add(new Section(new Station("양재역"), new Station("매봉역"), new WeightDivision("1/1")));
+
+        sections.add(new Section(new Station("강남역"), new Station("양재역"), new WeightDivision("2/8")));
+        sections.add(new Section(new Station("양재역"), new Station("양재시민의숲역"), new WeightDivision("10/3")));
+    }
+
+    public static List<Section> sections(){
+        return Collections.unmodifiableList(sections);
+    }
+
+    public static void calculate(List<Station> result, WeightCode weightCode) {
+
+    }
+}
