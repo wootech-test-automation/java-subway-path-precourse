ec0f438071b51aed3deca2e7474baf0c4bccfebd
JaehongDev <println@kakao.com>
2022-12-10 18:01:43 +0900

    refacor (Line) : 사용하지 않는 기능 제거

--- src/main/java/subway/domain/line/Line.java
+++ src/main/java/subway/domain/line/Line.java
@@ -5,14 +5,18 @@ import subway.domain.station.Station;
 
 public class Line {
     private final String name;
+    private final List<Station> stations;
 
     public Line(String name, List<Station> stations) {
         this.name = name;
+        this.stations = stations;
     }
 
     public String getName() {
         return name;
     }
 
-    // 추가 기능 구현
+    public List<Station> getStations() {
+        return stations;
+    }
 }
--- src/test/java/subway/domain/LineTest.java
+++ src/test/java/subway/domain/LineTest.java
@@ -1,7 +1,6 @@
 package subway.domain;
 
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -12,11 +11,10 @@ import subway.domain.station.Station;
 class LineTest {
 
     @Test
-    void 상행역이_종행역보다_앞에_와야합니다(){
+    void 상행역이_종행역보다_앞에_와야합니다() {
         var line = new Line("3호선", List.of
                 (new Station("교대역"), new Station("남부터미널역"), new Station("양재역"), new Station("매봉역")));
 
-        Assertions.assertThat(line.compareOriginalStationIndexBeforeTargetStation(new Station("매봉역"), new Station("교대역"))).isFalse();
     }
 
 }
