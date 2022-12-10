eacc080ec3faa6b77d98331d4d31a573769914bb
JaehongDev <println@kakao.com>
2022-12-10 16:53:10 +0900

    feature (WeightCode) : 가중치를 구분하는 기능 구현

---
+++ src/main/java/subway/domain/weight/WeightCode.java
@@ -0,0 +1,11 @@
+package subway.domain.weight;
+
+public enum WeightCode {
+    TIME("분"),
+    DISTANCE("km");
+
+    private final String message;
+    WeightCode(String message) {
+        this.message = message;
+    }
+}
