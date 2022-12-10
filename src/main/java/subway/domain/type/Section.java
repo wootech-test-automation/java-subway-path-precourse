b43b2d06be2589af2440e792dd3d5488131aecf8
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-10 14:01:09 +0900

    feat: add section domain

---
+++ src/main/java/subway/domain/type/Section.java
@@ -0,0 +1,49 @@
+package subway.domain.type;
+
+import java.util.Objects;
+
+public class Section {
+    private final Line line;
+    private final int distance;
+    private final int time;
+
+
+    public Section(Line line, int distance, int time) {
+        this.line = line;
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public static Section of(Line line, int distance, int time) {
+        return new Section(line,distance, time);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Section section = (Section) o;
+        return distance == section.distance && time == section.time && Objects.equals(line, section.line);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(line, distance, time);
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public int getTime() {
+        return time;
+    }
+}
