f91f61949c1b8752e8f5c6a704fdb53d440c1235
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-12-10 16:25:25 +0900

    feat: add path feature keyword

---
+++ src/main/java/subway/view/keyword/PathFeatureKeyword.java
@@ -0,0 +1,23 @@
+package subway.view.keyword;
+
+import java.util.Arrays;
+
+public enum PathFeatureKeyword {
+    DISTANCE("1"),
+    TIME("2"),
+    BACK("B");
+
+    private static final String ERROR_INPUT_MESSAGE = "1,2,B만 입력해주세요.";
+    private final String keyword;
+
+    PathFeatureKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    public static PathFeatureKeyword nameOf(String keyword) {
+        return Arrays.stream(PathFeatureKeyword.values())
+                .filter(pathFeatureKeyword -> keyword.equals(pathFeatureKeyword.keyword))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException(ERROR_INPUT_MESSAGE));
+    }
+}
